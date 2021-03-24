10ed6943fc3598da22d89906db01b4439ba611f4
martian1431 <pmalope@student.wethinkcode.co.za>
2021-03-24 14:03:16 +0200

    automated login for facebook, seem to be working but it was tricky at first because I couldn't select the id for the login since it keeps changing everytime I load the page

--- src/test/java/stepDefinitions/Annotation.java
+++ src/test/java/stepDefinitions/Annotation.java
@@ -1,39 +1,53 @@
 package stepDefinitions;
 
-import cucumber.api.PendingException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Annotation {
 
+	WebDriver driver = null;
+
 	@Given("^I am on Facebook login page$")
 	public void goToFacebookLoginPage() throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+//		throw new PendingException();
+		driver = new FirefoxDriver();
+		driver.navigate().to("https://www.facebook.com/");
 	}
 
 	@When("^I enter username as \"([^\"]*)\"$")
-	public void enterUsername(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+	public void enterUsername(String username) throws Throwable {
+		driver.findElement(By.id("email")).sendKeys(username);
 	}
 
 	@When("^I enter password as \"([^\"]*)\"$")
-	public void enterPassword(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+	public void enterPassword(String password) throws Throwable {
+		driver.findElement(By.id("pass")).sendKeys(password);
+		driver.findElement(By.name("login")).click();
 	}
 
 	@Then("^Login should fail$")
 	public void checkFail() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		if (driver.getCurrentUrl().equalsIgnoreCase("https://www.facebook.com/login.php?login_attempt=1&lwv=110")) {
+			System.out.println("Test1 Pass");
+		} else {
+			System.out.println("Test1 Failed");
+		}
+		driver.close();
 	}
 
 	@Then("^Relogin option should be available$")
 	public void checkRelogin() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		if (driver.getCurrentUrl().equalsIgnoreCase("https://www.facebook.com/login.php?login_attempt=1&lwv=110")) {
+			System.out.println("Test2 Pass");
+		} else {
+			System.out.println("Test2 Failed");
+		}
+		driver.close();
 	}
 }
