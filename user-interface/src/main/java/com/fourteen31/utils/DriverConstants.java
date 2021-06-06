b9adf3b0fba1012f17a711fddf9dded320d32fc8
Phetho Malope <pmalope@student.wethinkcode.co.za>
2021-06-06 22:28:37 +0200

    Defining driver constants

---
+++ user-interface/src/main/java/com/fourteen31/utils/DriverConstants.java
@@ -0,0 +1,18 @@
+package com.fourteen31.utils;
+
+public class DriverConstants {
+    //Driver
+    public static final String CHROME_DRIVER_WINDOWS = "webdriver.chrome.driver";
+    public static final String GECKO_DRIVER_WINDOWS = "webdriver.gecko.driver";
+    public static final String EDGE_DRIVER_WINDOWS = "webdriver.edge.driver";
+
+    //Driver Path Windows
+    public static final String CHROME_DRIVER_PATH_WINDOWS = "webdriver.chrome.driver.path.windows";
+    public static final String GECKO_DRIVER_PATH_WINDOWS = "webdriver.gecko.driver.path.windows";
+    public static final String EDGE_DRIVER_PATH_WINDOWS = "webdriver.edge.driver.path.windows";
+
+    //Driver Path Unix/Linux
+    public static final String CHROME_DRIVER_PATH_UNIX = "webdriver.chrome.driver.path.unix";
+    public static final String FIREFOX_DRIVER_PATH_UNIX = "webdriver.gecko.driver.path.unix";
+    public static final String EDGE_DRIVER_PATH_UNIX = "webdriver.edge.driver.path.unix";
+}
